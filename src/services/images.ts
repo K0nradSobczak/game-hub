2d03e18536d91a75a3d704f830ec6145525b56a8
Konrad Sobczak <konradsobczak14@gmail.com>
2025-01-05 22:20:37 +0100

    no image

--- src/services/images.ts
+++ src/services/images.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/GameHub Resources/Image Placeholder/no-image-placeholder-6f3882e0.webp';
+
 const getCroppedImage = (URL: string) => {
-  if(!URL) return '';
+  if(!URL) return noImage;
   const target = 'media/';
   const index = URL.indexOf('media/') + target.length;
   return URL.slice(0, index) + 'crop/600/400/' + URL.slice(index);
